f46e024ac106aea64320eb6564173375c291b84a
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-04-13 18:57:19 -0300

    implementation of application endpoints

--- src/main/java/one/digitalinnovation/crud/meetingroom/meetingroom/exception/GlobalExceptionHandler.java
+++ src/main/java/one/digitalinnovation/crud/meetingroom/meetingroom/exception/GlobalExceptionHandler.java
@@ -13,13 +13,21 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({ResourceNotFoundException.class})
     public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        ErrorDetails errorDetails = ErrorDetails.builder()
+                .timestamp(new Date())
+                .message(ex.getMessage())
+                .details(request.getDescription(false))
+                .build();
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({Exception.class})
     public ResponseEntity<?> globalExceptionHandle(ResourceNotFoundException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        ErrorDetails errorDetails = ErrorDetails.builder()
+                .timestamp(new Date())
+                .message(ex.getMessage())
+                .details(request.getDescription(false))
+                .build();
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
